93b243f61bb5493c8de603cfc7879f6624be3388
Nicolas Baguet <nyco.baguet@gmail.com>
2024-10-16 09:34:33 +0200

    ajout bouton rules

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const app = {
         dice.play.addEventListener("click", dice.playGame);
         dice.stop.addEventListener("click", dice.stopGame);
         dice.new.addEventListener("click", dice.resetGame);
+        dice.rules.addEventListener("click", dice.rulesGame);
     }
 }
 app.init();
--- js/dice.js
+++ js/dice.js
@@ -7,6 +7,7 @@ export const dice = {
     play: document.querySelector("#play"),
     stop: document.querySelector("#stop"),
     new: document.querySelector("#new"),
+    rules: document.querySelector("#rules"),
     launcheDice: function (min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     },
@@ -32,7 +33,9 @@ export const dice = {
     },
     stopGame: function () {
         dice.play.setAttribute("disabled", "");
-        dice.play.style.opacity = "0.8";
+        dice.stop.setAttribute("disabled", "");
+        dice.stop.style.opacity = "0.7";
+        dice.play.style.opacity = "0.7";
         while (dice.totalDealer < 18) {
             dice.totalDealer += dice.createDice("#dealer");
             document.querySelector("#score #dealerScore p").textContent = `Score Banque : ${dice.totalDealer}`
@@ -44,7 +47,7 @@ export const dice = {
         } else if (dice.totalDealer > dice.totalPlayer) {
             message.show(`Et c'est perdu ! la banque as fait plus que ton ${dice.totalPlayer} avec son ${dice.totalDealer}`, "error")
         } else if (dice.totalDealer === dice.totalPlayer) {
-            message.show(`Et c'est perdu, la banque as fait autant que toi avec ${dice.totalDealer}`, "error")
+            message.show(`Et c'est perdu ! la banque as fait autant que toi avec ${dice.totalDealer}`, "error")
         } else {
             message.show(`Et c'est gagné ! La banque as fait moins que ton ${dice.totalPlayer} avec son ${dice.totalDealer}`, "success")
         }
@@ -55,10 +58,15 @@ export const dice = {
             dice.remove();
         }
         dice.play.removeAttribute("disabled", "");
+        dice.stop.removeAttribute("disabled", "");
+        dice.stop.style.opacity = "1";
         dice.play.style.opacity = "1";
         dice.totalPlayer = 0;
         dice.totalDealer = 0;
         document.querySelector("#score #dealerScore p").textContent = `Score Banque : ${dice.totalDealer}`
         document.querySelector("#score #playerScore p").textContent = `Score Joueur : ${dice.totalPlayer}`
+    },
+    rulesGame: function () {
+        message.show(`Pour gagner, il faut lancer des dés pour avoir un meilleur score que la banque, sans jamais dépasser 21.`, "info", 10000)
     }
 }
